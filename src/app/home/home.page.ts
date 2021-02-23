8ef6cb6fe151c76f1abfed31a26cf75d3abca728
Jorge Rangel <jorgearturo99@live.com.mx>
2021-02-23 15:03:14 -0600

    First commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,67 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  password: string;
+  username: string;
+  ip: string;
+  messagge: string;
 
-  constructor() {}
+  constructor(
+    private alertCon: AlertController,
+    public httpClient: HttpClient
+  ) {}
 
+  async onLogin() {
+    if (
+      this.password == undefined ||
+      this.username == undefined ||
+      this.ip == undefined
+    ) {
+      const alertUndefined = await this.alertCon.create({
+        header: 'Error',
+        message: 'Llene todos los datos',
+        buttons: ['Ok'],
+      });
+
+      await alertUndefined.present();
+    } else {
+      let postData = {
+        password: this.password,
+        userName: this.username,
+      };
+
+      const headers = new HttpHeaders()
+        .set('Content-Type', 'application/json')
+        .set('Cache-Control', 'no-store')
+        .set('Pragma', 'no-cache')
+        .set('Expires', 'Sat, 01 Jan 2000 00:00:00 GMT');
+
+      this.httpClient
+        .post(`http://${this.ip}`, postData, {
+          headers: headers,
+        })
+        .subscribe(
+          async (data) => {
+            console.log(data);
+            const alert = await this.alertCon.create({
+              header: 'Error',
+              message: 'Login success',
+              buttons: ['Ok'],
+            });
+
+            await alert.present();
+          },
+          async (error) => {
+            console.log(error);
+            const alert = await this.alertCon.create({
+              header: 'Error',
+              message: 'Unauthorized',
+              buttons: ['Ok'],
+            });
+
+            await alert.present();
+          }
+        );
+    }
+  }
 }
